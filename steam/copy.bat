9f186f20004073dfa2ebbf59ed0c4144e9780a82
Iamasink <iamasink@proton.me>
2024-04-10 12:13:59 +0100

    steam input f

---
+++ steam/copy.bat
@@ -0,0 +1,6 @@
+@REM this copies the configuration for Steam input desktop
+@REM remove the local copy, /S remove files in directory, /Q quiet mode (dont ask for confirmation)
+rmdir 413080 /S /Q
+@REM copy from steam config
+robocopy "C:\Program Files (x86)\Steam\steamapps\common\Steam Controller Configs\341416160\config\413080" "./413080"
+timeout /t 10
