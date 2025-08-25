5c7e0048f24a95508b9d663d1fad0f95caad78dc
iamasink <iamasink@proton.me>
2025-08-25 14:48:47 +0100

    fix

--- nilesoftshell/copyandcommit.bat
+++ nilesoftshell/copyandcommit.bat
@@ -1,6 +1,6 @@
 @REM remove the local copy, /S remove files in directory, /Q quiet mode (dont ask for confirmation)
-rmdir Nilesoft Shell /S /Q
-@REM copy from steam config
+rmdir "Nilesoft Shell" /S /Q
+@REM copy from config
 robocopy "C:\Program Files\Nilesoft Shell" "./Nilesoft Shell" *.nss /s
 git add *.nss
 git commit -m "Updated nilesoft shell config"
