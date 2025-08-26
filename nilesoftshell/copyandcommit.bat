c1976668c500ae06f8b0703fa026003e8f126b0d
iamasink <iamasink@proton.me>
2025-08-26 06:05:13 +0100

    commit all

--- nilesoftshell/copyandcommit.bat
+++ nilesoftshell/copyandcommit.bat
@@ -1,7 +1,8 @@
 @REM remove the local copy, /S remove files in directory, /Q quiet mode (dont ask for confirmation)
 rmdir "Nilesoft Shell" /S /Q
 @REM copy from config
-robocopy "C:\Program Files\Nilesoft Shell" "./Nilesoft Shell" *.nss /s
-git add *.nss
+robocopy "C:\Program Files\Nilesoft Shell" "./Nilesoft Shell" * /s
+cd "./Nilesoft Shell"
+git add *
 git commit -m "Updated nilesoft shell config"
 timeout /t 30
